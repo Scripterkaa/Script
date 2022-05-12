046c3b233dc159e97282bda3d643fc26df2ad9b0
Scripterkaa <88961862+Scripterkaa@users.noreply.github.com>
2022-05-13 00:18:29 +0700

    Update case.lua

--- case.lua
+++ case.lua
@@ -249,6 +249,50 @@ function fullyfarm()
         end
     end)
 end
+function fpsboost()
+    pcall(function()
+        local a = game
+        local b = a.Workspace
+        local c = a.Lighting
+        local d = b.Terrain
+        d.WaterWaveSize = 0
+        d.WaterWaveSpeed = 0
+        d.WaterReflectance = 0
+        d.WaterTransparency = 0
+        c.GlobalShadows = false
+        c.FogEnd = 9e9
+        c.Brightness = 0
+        settings().Rendering.QualityLevel = "Level01"
+        for e, f in pairs(a:GetDescendants()) do
+        if f:IsA("Part") or f:IsA("Union") or f:IsA("CornerWedgePart") or f:IsA("TrussPart") then
+            f.Material = "Plastic"
+            f.Reflectance = 0
+        elseif f:IsA("Decal") or f:IsA("Texture") then
+            f.Transparency = 0
+        elseif f:IsA("ParticleEmitter") or f:IsA("Trail") then
+            f.Lifetime = NumberRange.new(0)
+        elseif f:IsA("Explosion") then
+            f.BlastPressure = 0
+            f.BlastRadius = 0
+        elseif f:IsA("Fire") or f:IsA("SpotLight") or f:IsA("Smoke") or f:IsA("Sparkles") then
+            f.Enabled = false
+        elseif f:IsA("MeshPart") then
+            f.Material = "Plastic"
+            f.Reflectance = 0
+            f.TextureID = 10385902758728957
+        end
+        end
+        for e, g in pairs(c:GetChildren()) do
+        if
+            g:IsA("BlurEffect") or g:IsA("SunRaysEffect") or g:IsA("ColorCorrectionEffect") or g:IsA("BloomEffect") or
+                g:IsA("DepthOfFieldEffect")
+            then
+            g.Enabled = false
+        end
+        end
+        sethiddenproperty(game.Lighting, "Technology", "Compatibility")
+    end)
+end
 
 function jo()
     pcall(function()
@@ -372,6 +416,9 @@ mm:addToggle("Drop Requiem Arrow",nil,function(go)
     else
         _G.drop = false
     end
+end)
+mm:addButton("FPS Boost",function()
+    fpsboost()
 end)
 mm:addToggle("Anti AFK",nil,function(go)
     if go then
